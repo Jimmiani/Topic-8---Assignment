79046e97ef00506c4a0061cda3c0c48ba69f2f90
Emmett Cornies <339761330@GoTVDSB.ca>
2024-10-28 09:39:31 -0400

    Add project files.

---
+++ Topic 8 - Assignment/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_8___Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List <string> colours = new List <string> ();
+        }
+    }
+}
