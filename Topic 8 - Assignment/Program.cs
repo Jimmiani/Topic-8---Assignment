09f493c353fecb59eeb978e5fa1fa16736f661ee
Emmett Cornies <339761330@GoTVDSB.ca>
2024-10-29 09:39:31 -0400

    not done

--- Topic 8 - Assignment/Program.cs
+++ Topic 8 - Assignment/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace Topic_8___Assignment
+﻿using System.Globalization;
+
+namespace Topic_8___Assignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            List <string> colours = new List <string> ();
+            Random generator = new Random();
+            
+            List <string> colours = new List <string>();
+            string enteredColour;
+            int numOfColours = 5, randNum = generator.Next(numOfColours - 1);;
+            for (int i = 0; i < numOfColours; i++)
+            {
+                Console.Write("Enter a colour: ");
+                enteredColour = Console.ReadLine();
+                colours.Add(enteredColour);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < numOfColours;i++)
+            {
+                if (i == (numOfColours - 1))
+                    Console.Write(colours[i] + ".");
+                else
+                    Console.Write(colours[i] + ", ");
+            }
+            Console.WriteLine();
+            Console.Write("Here's a random number, and it's corresponding colour: ");
+            Console.WriteLine(randNum + ", " + colours[randNum]);
+            Console.ReadLine();
         }
     }
 }
